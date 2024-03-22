881efa62b360c7afbe369d8f389c7d7868bb43b9
Afath <113617035+Afath10@users.noreply.github.com>
2024-03-22 17:07:24 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-0b90e394-0680-49a7-8ad6-70c25c129b69/)
 
 
 echo "$curl_output" >> Update.tsx
